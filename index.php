f315859df9e04abe4b293d3af2ca02b6af5bda4c
FO-nTTaX <fo.nttax@yahoo.de>
2016-05-20 17:14:02 +0200

    some weird thing happened with encoding

--- index.php
+++ index.php
@@ -136,8 +136,8 @@ while ($row = mysql_fetch_assoc ($r))
 		<div class="whitebox">
 			<div class="content how-to">
 				<p>Contributing to the wiki is actually pretty easy and keep in mind that every more-correct-than-wrong contribution is valuable no matter how small. </p>
-				<p>When you visit Liquipedia, consider adding to it or correcting something, it doesn’t have to take up much of your time and effort and it will help other visitors like yourself and Liquipedia as a whole.</p>
-				<p>Many people start by fixing typos, which is actually the easiest way to contribute. You just have to create an account—if you don’t have one already—log in, click edit, find and fix the typo, click save, and you are done.</p>
+				<p>When you visit Liquipedia, consider adding to it or correcting something, it doesn't have to take up much of your time and effort and it will help other visitors like yourself and Liquipedia as a whole.</p>
+				<p>Many people start by fixing typos, which is actually the easiest way to contribute. You just have to create an account—if you don't have one already—log in, click edit, find and fix the typo, click save, and you are done.</p>
 				<p>Another thing that many contributors start with is keeping tournament results up to date while the tournament is ongoing. Most of the times the pages are already set up by one of the more experienced contributors, and you just have to fill in the results as they happen. Filling a bracket is pretty straightforward. You log in, click on edit, find the bracket, update scores and fill in names. If you are unsure, just look at how it was done on other pages, either by just looking at the page itself, or by clicking edit to examine how the page was created. In general, looking at how things are done on other pages gives you a good idea of how you can do it yourself. </p>
 				<p>If you feel comfortable with wiki editing or if you want to learn things that are more advanced, feel free to browse our “How to contribute�? sections you can find in the menus on the left of the wiki pages. You can find us in our IRC channel where other contributors can help you. <a href="http://webchat.quakenet.org/?channels=#liquipedia" target="_blank">#liquipedia on QuakeNet</a>.</p>
 				<h3>Logging in and registering</h3>
@@ -149,11 +149,11 @@ while ($row = mysql_fetch_assoc ($r))
 				<h3>Areas to help out with</h3>
 				<p>There are multiple things one can do to help out with on the wikis. Besides fixing typos or entering results you can:</p>
 				<ul>
-					<li>List an interview a player has done on the player’s page.</li>
-					<li>Add social media links on a team’s or player’s page.</li>
-					<li>Fill out a player’s page with information from interviews they’ve done and add references.</li>
+					<li>List an interview a player has done on the player's page.</li>
+					<li>Add social media links on a team's or player's page.</li>
+					<li>Fill out a player's page with information from interviews they've done and add references.</li>
 					<li>Write part of the history of an organization, tournament, player, or team and add references.</li>
-					<li>Update a player’s or team’s result pages with recent results.</li>
+					<li>Update a player's or team's result pages with recent results.</li>
 					<li>Add the times when a match starts to brackets or groups.</li>
 					<li>Add general game information, either to an existing article or make a new one for a topic that is missing.</li>
 					<li>Update strategy pages and create pages for new strategies.</li>

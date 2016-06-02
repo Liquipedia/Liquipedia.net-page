db94079f8d5ef8bc538e31b6e1bef4497e28e590
Richard Stanway <r1ch@teamliquid.net>
2016-06-03 02:37:44 +0900

    Update GA code

--- index.php
+++ index.php
@@ -196,17 +196,17 @@ while ($row = mysql_fetch_assoc ($r))
 				}
 			});
 		</script>
-		<script type="text/javascript">
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', 'UA-576564-4']);
-			_gaq.push(['_trackPageview']);
 
-			(function() {
-				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-576564-4', 'auto');
+		  ga('send', 'pageview');
 		</script>
+
 		<!-- Quantcast Tag -->
 		<script type="text/javascript">
 			var _qevents = _qevents || [];

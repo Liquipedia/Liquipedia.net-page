5503a37f10c9691d16ab06ec6dfdae1a056f22b9
FO-nTTaX <fo.nttax@yahoo.de>
2016-10-14 13:14:45 +0200

    reorder wikis

--- includes/wikis.php
+++ includes/wikis.php
@@ -14,40 +14,40 @@ $wikis = array (
 		'background-color' => '#e5ccb6',
 		'api' => $baseurl . '/dota2/api.php',
 	),
-	'starcraft2' => array (
-		'name' => 'StarCraft II',
-		'background-color' => '#b6cfe5',
-		'api' => $baseurl . '/starcraft2/api.php',
-	),
 	'counterstrike' => array (
 		'name' => 'Counter-Strike',
 		'background-color' => '#cde5b6',
 		'api' => $baseurl . '/counterstrike/api.php',
+	),
+	'starcraft2' => array (
+		'name' => 'StarCraft II',
+		'background-color' => '#b6cfe5',
+		'api' => $baseurl . '/starcraft2/api.php',
 	),
 	'hearthstone' => array (
 		'name' => 'Hearthstone',
 		'background-color' => '#e5d8b6',
 		'api' => $baseurl . '/hearthstone/api.php',
 	),
-	'starcraft' => array (
-		'name' => 'Brood War',
-		'background-color' => '#dbe1e5',
-		'api' => $baseurl . '/starcraft/api.php',
-	),
-	'smash' => array (
-		'name' => 'Smash',
-		'background-color' => '#b6e5c7',
-		'api' => $baseurl . '/smash/api.php',
+	'overwatch' => array (
+		'name' => 'Overwatch',
+		'background-color' => '#e5b6c0',
+		'api' => $baseurl . '/overwatch/api.php',
 	),
 	'heroes' => array (
 		'name' => 'Heroes',
 		'background-color' => '#b9b6e5',
 		'api' => $baseurl . '/heroes/api.php',
 	),
-	'overwatch' => array (
-		'name' => 'Overwatch',
-		'background-color' => '#e5b6c0',
-		'api' => $baseurl . '/overwatch/api.php',
+	'smash' => array (
+		'name' => 'Smash',
+		'background-color' => '#b6e5c7',
+		'api' => $baseurl . '/smash/api.php',
+	),
+	'starcraft' => array (
+		'name' => 'Brood War',
+		'background-color' => '#dbe1e5',
+		'api' => $baseurl . '/starcraft/api.php',
 	),
 	'rocketleague' => array (
 		'name' => 'Rocket League',

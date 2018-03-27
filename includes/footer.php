c690bb5ed8c11ed7fe3cbdec33b3553df618c8dc
FO-nTTaX <fo.nttax@yahoo.de>
2018-03-27 18:22:18 +0200

    2018 landing page (thanks wo1fwood)

--- includes/footer.php
+++ includes/footer.php
@@ -2,9 +2,9 @@
 	<div class="footer-content">
 		<div class="footer-block">
 			<div class="footer-group">
-			<a href="http://liquipedia.net/">
+			<a href="<?php echo $baseurl; ?>">
 				<svg class="footer-logo" viewBox="0 0 128 116.18">
-					<use xlink:href="images/logo_vert.svg#brand"/>
+					<use xlink:href="<?php echo $baseurl; ?>/images/logo_vert.svg#brand"/>
 				</svg>
 				<span class="screen-reader-text">Go To Liquipedia Homepage</span>
 			</a>
@@ -12,28 +12,28 @@
 		--><div class="footer-group">
 				<h1 id="primary-wikis">Wikis</h1>
 				<ul class="display-list" aria-labeledby="primary-wikis">
-					<li><a href="/commons/">Commons</a></li>
-					<li><a href="/starcraft/">Brood War</a></li>
-					<li><a href="/counterstrike/">Counter-Strike</a></li>
-					<li><a href="/dota2/">Dota 2</a></li>
-					<li><a href="/hearthstone/">Hearthstone</a></li>
-					<li><a href="/heroes/">Heroes of the Storm</a></li>
-					<li><a href="/overwatch/">Overwatch</a></li>
-					<li><a href="/rocketleague/">Rocket League</a></li>
-					<li><a href="/smash/">Smash</a></li>
-					<li><a href="/starcraft2/">StarCraft II</a></li>
+					<li><a href="<?php echo $baseurl; ?>/commons/">Commons</a></li>
+					<li><a href="<?php echo $baseurl; ?>/starcraft/">Brood War</a></li>
+					<li><a href="<?php echo $baseurl; ?>/counterstrike/">Counter-Strike</a></li>
+					<li><a href="<?php echo $baseurl; ?>/dota2/">Dota 2</a></li>
+					<li><a href="<?php echo $baseurl; ?>/hearthstone/">Hearthstone</a></li>
+					<li><a href="<?php echo $baseurl; ?>/heroes/">Heroes of the Storm</a></li>
+					<li><a href="<?php echo $baseurl; ?>/overwatch/">Overwatch</a></li>
+					<li><a href="<?php echo $baseurl; ?>/rocketleague/">Rocket League</a></li>
+					<li><a href="<?php echo $baseurl; ?>/smash/">Smash</a></li>
+					<li><a href="<?php echo $baseurl; ?>/starcraft2/">StarCraft II</a></li>
 				</ul>
 			</div><!--
 			--><div class="footer-group">
 				<h1 id="alpha-wikis">Alpha Wikis</h1>
 				<ul class="display-list" aria-labeledby="alpha-wikis">
-					<li><a href="/fighters/">Fighting Games</a></li>
-					<li><a href="/leagueoflegends/">League of Legends</a></li>
-					<li><a href="/quake/">Quake</a></li>
-					<li><a href="/rainbowsix/">Rainbow Six</a></li>
-					<li><a href="/teamfortress/">Team Fortress</a></li>
-					<li><a href="/warcraft/">Warcraft III</a></li>
-					<li><a href="/worldofwarcraft/">World of Warcraft</a></li>
+					<li><a href="<?php echo $baseurl; ?>/fighters/">Fighting Games</a></li>
+					<li><a href="<?php echo $baseurl; ?>/leagueoflegends/">League of Legends</a></li>
+					<li><a href="<?php echo $baseurl; ?>/quake/">Quake</a></li>
+					<li><a href="<?php echo $baseurl; ?>/rainbowsix/">Rainbow Six</a></li>
+					<li><a href="<?php echo $baseurl; ?>/teamfortress/">Team Fortress</a></li>
+					<li><a href="<?php echo $baseurl; ?>/warcraft/">Warcraft III</a></li>
+					<li><a href="<?php echo $baseurl; ?>/worldofwarcraft/">World of Warcraft</a></li>
 				</ul>
 			</div><!--
 			--><div class="footer-group">
@@ -50,7 +50,7 @@
 				<h1 id="about-us">Liquipedia</h1>
 				<ul class="display-list" aria-labeledby="about-us">
 					<li id="footer-places-about"><a href="/counterstrike/Liquipedia:About" title="Liquipedia:About">About</a></li>
-					<li><a href="http://liquipedia.net/discord" target="_blank" rel="noopener noreferrer">Chat with Us</a></li>
+					<li><a href="<?php echo $baseurl; ?>/discord" target="_blank" rel="noopener noreferrer">Chat with Us</a></li>
 					<li id="footer-places-disclaimer"><a href="/counterstrike/Liquipedia:General_disclaimer" title="Liquipedia:General disclaimer">Disclaimers</a></li>
 					<li id="footer-places-privacy"><a href="/counterstrike/Liquipedia:Privacy_policy" title="Liquipedia:Privacy policy">Privacy policy</a></li>
 					<li><a href="http://www.teamliquid.net/forum/website-feedback/94785-liquipedia-feedback-thread">Post Feedback</a></li>

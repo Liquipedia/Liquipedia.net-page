3b643fb57a790be343770264eee575b70ac4993b
FO-nTTaX <fo.nttax@yahoo.de>
2019-09-23 20:18:39 +0200

    replace IRC with Discord in text

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ foreach ( $alphawikis as $wiki_key => $wiki ) {
 
 	Hi you, yes you who's looking at our source code! Are you a website specialist?
 	We are looking for people to help us with our templates, especially with mobile development.
-	If you want to help, be sure to visit us us on discord (https://liquipedia.net/discord), or send us
+	If you want to help, be sure to visit us us on discord (https://discord.gg/liquipedia), or send us
 	an email to liquipedia <at> teamliquid <dot> net!
 -->
 <html lang="en">
@@ -214,7 +214,7 @@ foreach ( $alphawikis as $wiki_key => $wiki ) {
 				<p>When you visit Liquipedia, consider adding to it or correcting something, it doesn't have to take up much of your time and effort and it will help other visitors like yourself and Liquipedia as a whole.</p>
 				<p>Many people start by fixing typos, which is actually the easiest way to contribute. You just have to create an account&mdash;if you don't have one already&mdash;log in, click edit, find and fix the typo, click save, and you are done.</p>
 				<p>Another thing that many contributors start with is keeping tournament results up to date while the tournament is ongoing. Most of the times the pages are already set up by one of the more experienced contributors, and you just have to fill in the results as they happen. Filling a bracket is pretty straightforward. You log in, click on edit, find the bracket, update scores and fill in names. If you are unsure, just look at how it was done on other pages, either by just looking at the page itself, or by clicking edit to examine how the page was created. In general, looking at how things are done on other pages gives you a good idea of how you can do it yourself. </p>
-				<p>If you feel comfortable with wiki editing or if you want to learn things that are more advanced, feel free to browse our &quot;How to contribute&quot; sections you can find in the menus on the left of the wiki pages. You can <a rel="noopener" href="https://liquipedia.net/discord" target="_blank">find us on our Discord server</a> where other contributors can help you.</p>
+				<p>If you feel comfortable with wiki editing or if you want to learn things that are more advanced, feel free to browse our &quot;How to contribute&quot; sections you can find in the menus on the left of the wiki pages. You can <a rel="noopener" href="https://discord.gg/liquipedia" target="_blank">find us on our Discord server</a> where other contributors can help you.</p>
 				<h4>Logging in and registering</h4>
 				<p>To log in and edit Liquipedia you need a TeamLiquid account. To register an account, click on the &quot;<a href="https://tl.net/mytlnet/register" target="_blank">create account</a>&quot; link on any wiki page, just remember to follow the instructions and complete the registration.<br />
 					Once you have an account go click on the log in box in the top right and enter your details or if logged in on any of the three sites mentioned just click on the TL quick log in link. </p>
@@ -246,7 +246,7 @@ foreach ( $alphawikis as $wiki_key => $wiki ) {
 		<div class="footer">
 			<div class="content">
 				<div class="icon-list">
-					<a aria-label="Discord" rel="noopener" href="https://liquipedia.net/discord" target="_blank"><i class="lp-icon lp-discord"></i></a>
+					<a aria-label="Discord" rel="noopener" href="https://discord.gg/liquipedia" target="_blank"><i class="lp-icon lp-discord"></i></a>
 					<a aria-label="Twitter" rel="noopener" href="https://twitter.com/LiquipediaNet" target="_blank"><i class="lp-icon lp-twitter"></i></a>
 					<a aria-label="Facebook" rel="noopener" href="https://www.facebook.com/Liquipedia" target="_blank"><i class="lp-icon lp-facebook"></i></a>
 					<a aria-label="YouTube" rel="noopener" href="https://www.youtube.com/user/Liquipedia" target="_blank"><i class="lp-icon lp-youtube"></i></a>

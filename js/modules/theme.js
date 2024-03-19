5e2c83b88e074124e3486e935acbb74743a06b60
Alex Winkler <fo.nttax@yahoo.de>
2024-03-19 18:32:48 +0100

    change dark theme toggle to account for new default behaviour

--- js/modules/theme.js
+++ js/modules/theme.js
@@ -1,68 +1,89 @@
 export class Theme {
     constructor( htmlElement ) {
         this.htmlElement = htmlElement;
-        this.darkModeActive = null;
+        this.darkThemeActive = null;
         this.darkModeStorageKey = 'LiquipediaDarkMode';
+        this.darkThemeStorageKey = 'LiquipediaDarkTheme';
         this.lightThemeClass = 'theme--light';
         this.darkThemeClass = 'theme--dark';
         this.button = document.querySelector( '[data-component="theme-switch"]' );
+
+        // Transform old storage key to new storage key
+        // @todo remove after 2024-04-15
+        if ( window.localStorage.getItem( this.darkModeStorageKey ) !== null ) {
+            window.localStorage.setItem( this.darkThemeStorageKey, 'dark' );
+            window.localStorage.removeItem( this.darkModeStorageKey );
+        }
     }
 
     init() {
-        this.darkModeActive = JSON.parse( this.checkLocalStorage() );
-        if ( this.darkModeActive === null ) {
-            this.darkModeActive = this.htmlElement.classList.contains( this.darkThemeClass );
-        }
-        // this.toggleThemeClassOnBody();
+        /*
+         * The tracking is in this function because
+         * we only want to capture button clicks,
+         * not changes through localStorage events
+         */
+        this.button.addEventListener( 'click', ( event ) => {
+            event.stopPropagation();
+            const currentTheme = this.getCurrentTheme();
 
-        this.button.addEventListener( 'click', () => {
-            this.darkModeActive = !this.darkModeActive;
-            this.toggleThemeClassOnBody();
+            if ( currentTheme === 'light' ) {
+                this.setDarkTheme();
+            } else {
+                this.setLightTheme();
+            }
         } );
-
-        this.setupEventListener();
+        this.setupThemeListeners();
     }
 
-    setDarkMode( setLocalStorage = true ) {
-        this.htmlElement.classList.remove( this.lightThemeClass );
-        this.htmlElement.classList.add( this.darkThemeClass );
-        this.setLocalStorage( 'true' );
-        // this.setAriaPressed( 'true' );
-    }
-
-    setLightMode( setLocalStorage = true ) {
-        this.htmlElement.classList.remove( this.darkThemeClass );
-        this.htmlElement.classList.add( this.lightThemeClass );
-        this.setLocalStorage( null );
-        // this.setAriaPressed( 'false' );
-    }
-
-    toggleThemeClassOnBody() {
-        if ( this.darkModeActive ) {
-            this.setDarkMode();
-        } else {
-            this.setLightMode();
-        }
-    }
-
-    setLocalStorage( value ) {
-        window.localStorage.setItem( this.darkModeStorageKey, value );
-    }
-
-    checkLocalStorage() {
-        return window.localStorage.getItem( this.darkModeStorageKey );
-    }
-
-    setupEventListener() {
+    setupThemeListeners() {
         window.addEventListener( 'storage', ( event ) => {
-            if ( event.key === this.darkModeStorageKey ) {
-                this.darkModeActive = JSON.parse( event.newValue );
-                if ( event.newValue === 'true' ) {
-                    this.setDarkMode( false );
-                } else {
-                    this.setLightMode( false );
+            if ( event.key === this.darkThemeStorageKey ) {
+                if ( event.newValue === 'dark' ) {
+                    this.setDarkTheme( false );
+                } else if ( event.newValue === 'light' ) {
+                    this.setLightTheme( false );
                 }
             }
         } );
+
+        const osPreference = window.matchMedia( '( prefers-color-scheme: dark )' );
+        osPreference.addEventListener( 'change', ( event ) => {
+            if ( window.localStorage.getItem( this.darkThemeStorageKey ) === null ) {
+                if ( event.matches ) {
+                    this.setDarkTheme( false );
+                } else {
+                    this.setLightTheme( false );
+                }
+            }
+        } );
+    }
+
+    getCurrentTheme() {
+        const currentTheme = window.localStorage.getItem( this.darkThemeStorageKey );
+        if ( currentTheme === null ) {
+            if ( window.matchMedia( '( prefers-color-scheme: dark )' ).matches ) {
+                return 'dark';
+            }
+            return 'light';
+        }
+        return currentTheme;
+    }
+
+    setDarkTheme( setLocalStorage = true ) {
+        this.htmlElement.classList.remove( this.lightThemeClass );
+        this.htmlElement.classList.add( this.darkThemeClass );
+        if ( setLocalStorage ) {
+            window.localStorage.setItem( this.darkThemeStorageKey, 'dark' );
+        }
+        // this.setAriaPressed( 'true' );
+    }
+
+    setLightTheme( setLocalStorage = true ) {
+        this.htmlElement.classList.remove( this.darkThemeClass );
+        this.htmlElement.classList.add( this.lightThemeClass );
+        if ( setLocalStorage ) {
+            window.localStorage.setItem( this.darkThemeStorageKey, 'light' );
+        }
+        // this.setAriaPressed( 'false' );
     }
 }
